27161bceedebe189e929a69429d0d08c6db80624
Max Karpawich <max.karpawich@gmail.com>
2020-07-04 14:56:47 -0400

    feat: initial Bolt app

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,20 @@
-import { App } from "@slack/bolt";
+import { App } from '@slack/bolt';
+import { config } from 'dotenv';
+import _debug from 'debug';
 
+const debug = _debug('bot');
+
+if (process.env.NODE_ENV !== 'production') {
+  config();
+}
+
+const app:App = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+});
+
+(async () => {
+  // Start the app
+  await app.start(process.env.PORT || 3000);
+  debug('⚡️ Bolt app is running!');
+})();
