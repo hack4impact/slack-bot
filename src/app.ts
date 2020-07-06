90e38583590f5eda25a0888bb0732c6c7b465ead
Max Karpawich <max.karpawich@gmail.com>
2020-07-05 23:35:12 -0400

    feat: basic plugin system

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import { App } from '@slack/bolt';
 import _debug from 'debug';
-import connection from './util/connection';
+import db from './util/db';
 import config from './util/config';
+import loadPlugins from './util/plugin';
 
 const debug = _debug('bot');
 
@@ -10,22 +11,12 @@ const app:App = new App({
   token: config.slackBotToken,
 });
 
-// Give every listener MongoDB access
-app.use(async ({ next }) => {
-  await connection();
-  if (next) await next();
-});
-
-// Listens to incoming messages that contain "hello"
-app.message('hello123', async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  await say(`Hey there <@${message.user}>!`);
-});
-
 (async () => {
   debug('Boot sequence initiated...');
 
-  await connection();
+  await db();
+
+  await loadPlugins(app);
 
   // Start the app
   await app.start(config.port);
---
+++ src/util/plugin.ts
@@ -0,0 +1,46 @@
+import type { App } from '@slack/bolt';
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
+type PrefixFunction = (id:string) => string;
+
+interface PluginJSON {
+  name: string,
+  version: string,
+  slug: string,
+}
+
+type PluginFunction = (app:App, pre:PrefixFunction) => Promise<void>;
+
+interface Plugin {
+  json: PluginJSON,
+  func: PluginFunction
+}
+
+// Some TypeScript wizardy that dynamically loads plugins
+async function loadPlugins(app:App) {
+  const pluginFolders:string[] = await fs.readdir(join(__dirname, 'plugins'));
+  const loadPromises = [];
+  for (let i = 0; i < pluginFolders.length; i += 1) {
+    const pluginPath = `./plugins/${pluginFolders[i]}`;
+    loadPromises.push((async () => {
+      const json:PluginJSON = JSON.parse((await fs.readFile(join(__dirname, pluginPath, 'plugin.json'))).toString());
+      const func:PluginFunction = (await import(`${pluginPath}/plugin`)).default;
+      return { json, func };
+    })());
+  }
+  const plugins:Plugin[] = await Promise.all(loadPromises);
+  const pluginPromises = [];
+  for (let i = 0; i < plugins.length; i += 1) {
+    const plugin = plugins[i];
+    const pre:PrefixFunction = (id) => `${plugin.json.slug}_${id}`;
+    pluginPromises.push(plugin.func(app, pre));
+  }
+  await Promise.all(pluginPromises);
+}
+
+export type {
+  PrefixFunction, PluginJSON, PluginFunction, Plugin,
+};
+
+export default loadPlugins;
