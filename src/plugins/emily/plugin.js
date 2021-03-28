2e8b29c9088a771c4fe8537f97ec2c86af8a14d4
Emily Hodges <ehodges9@gatech.edu>
2021-03-28 18:34:41 -0400

    Changed plugin

--- src/plugins/emily/plugin.js
+++ src/plugins/emily/plugin.js
@@ -1,5 +1,5 @@
-export default async (app, pre) => {
-  app.message(pre('new'), async({message, say}) => {
-    await say('Hello, <@${message.user}>!')
+export default async (app) => {
+  app.message(':turtle:', async({message, say}) => {
+    await say(`Hello, <@${message.user}>! This is the helpful turtle bot. What can I help you with?`)
   })
 }
