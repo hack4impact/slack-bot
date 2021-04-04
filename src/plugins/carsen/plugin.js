aa1d572c0e5417a528241d68f2e3d4e3a3d37690
cmiller615 <carsenjakemiller@gmail.com>
2021-04-04 00:45:29 -0400

    pizza_msg fix attempt 2

--- src/plugins/carsen/plugin.js
+++ src/plugins/carsen/plugin.js
@@ -1,12 +1,12 @@
 export default async (app) => {
   app.message(('Does :pineapple: belong on :pizza:?'), async ({message, say}) => {
-    await say(pizza_msg)
+    await say(`Hello, <@${message.user}>! What a silly question. Try :bell_pepper: or :bacon: or heck, just :cheese:!!`)
   })
 }
 export default async (app) => {
   app.message(('Does pineapple belong on pizza?'), async ({message, say}) => {
-    await say(pizza_msg)
+    await say(`Hello, <@${message.user}>! What a silly question. Try :bell_pepper: or :bacon: or heck, just :cheese:!!`)
   })
 }
 
-var pizza_msg = `Hello, <@${message.user}>! What a silly question. Try :bell_pepper: or :bacon: or heck, just :cheese:!!`;
+//var pizza_msg = `Hello, <@${message.user}>! What a silly question. Try :bell_pepper: or :bacon: or heck, just :cheese:!!`;
