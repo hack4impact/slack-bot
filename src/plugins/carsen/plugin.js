944a1fc5e57210c5fdf8c6f372511941ef15ee54
cmiller615 <carsenjakemiller@gmail.com>
2021-04-04 00:35:44 -0400

    pizza pizza

--- src/plugins/carsen/plugin.js
+++ src/plugins/carsen/plugin.js
@@ -1,5 +1,12 @@
-export default async (app, pre) => {
-  app.message(pre('hello world'), async ({message, say}) => {
-    await say(`Hello, <@${message.user}>!`)
+export default async (app) => {
+  app.message(('Does :pineapple: belong on :pizza:?'), async ({message, say}) => {
+    await say(pizza_msg)
   })
 }
+export default async (app) => {
+  app.message(('Does pineapple belong on pizza?'), async ({message, say}) => {
+    await say(pizza_msg)
+  })
+}
+
+var pizza_msg = 'Hello, <@${message.user}>! What a silly question. Try :bell_pepper: or :bacon: or heck, just :cheese:!!';
