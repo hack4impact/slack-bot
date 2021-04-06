726e6254ea584f73df882ed12e8a9349659e18b7
cmiller615 <carsenjakemiller@gmail.com>
2021-04-06 12:34:59 -0400

    bug push for Max

--- src/plugins/carsen/plugin.js
+++ src/plugins/carsen/plugin.js
@@ -1,13 +1,10 @@
 export default async (app) => {
-  app.message((('Does :pineapple: belong on :pizza:?') || ('Does pineapple belong on pizza?')), async ({message, say}) => {
+  app.message(((`Does :pineapple: belong on :pizza:?`), async ({message, say}) => {
+    await say(`Hello, <@${message.user}>! What a silly question. Try :bell_pepper: or :bacon: or heck, just :cheese:!!`)
+  }),
+  app.message(((`Does pineapple belong on pizza?`), async ({message, say}) => {
     await say(`Hello, <@${message.user}>! What a silly question. Try :bell_pepper: or :bacon: or heck, just :cheese:!!`)
   })
 }
-// FIXME need new prefix for multiple commands.
-// export default async (app) => { 
-//   app.message(('Does pineapple belong on pizza?'), async ({message, say}) => {
-//     await say(`Hello, <@${message.user}>! What a silly question. Try :bell_pepper: or :bacon: or heck, just :cheese:!!`)
-//   })
-// }
 
 //var pizza_msg = `Hello, <@${message.user}>! What a silly question. Try :bell_pepper: or :bacon: or heck, just :cheese:!!`;
