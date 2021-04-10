7304701118a8e759ef31c7760174ec2051723b56
cmiller615 <carsenjakemiller@gmail.com>
2021-04-10 02:02:04 -0400

    meetingmins update

--- src/plugins/meetingminutes/plugin.js
+++ src/plugins/meetingminutes/plugin.js
@@ -1,10 +1,12 @@
 export default async (app) => {
-  app.message(((`Does :pineapple: belong on :pizza:?`), async ({message, say}) => {
-    await say(`Hello, <@${message.user}>! What a silly question. Try :bell_pepper: or :bacon: or heck, just :cheese:!!`)
-  }));
-  app.message(((`Does pineapple belong on pizza?`), async ({message, say}) => {
+  app.message(((`Update me`), async ({message, say}) => {
     await say(`Hello, <@${message.user}>! What a silly question. Try :bell_pepper: or :bacon: or heck, just :cheese:!!`)
   }));
 }
-
-//var pizza_msg = `Hello, <@${message.user}>! What a silly question. Try :bell_pepper: or :bacon: or heck, just :cheese:!!`;
+let msg_opener = await say(`Extra extra! Read all about it! Here are the most recent notes from the ` + date + ` ` + title + ` :rolled_up_newspaper:`);
+let msg_tldr = await say(`TLDR: ` + tldr);
+let msg_events = await say(`Events: ` + events);
+let title = await fetch(msg_title);
+let date = await fetch(msg_date);
+let tldr = await fetch(msg_tldr);
+let events = await fetch(msg_events);
