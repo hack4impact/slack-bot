e818f877e8027acb237e66dc219bc02a080d7d46
cmiller615 <carsenjakemiller@gmail.com>
2021-04-10 02:36:31 -0400

    meetingminutes var fixes

--- src/plugins/meetingminutes/plugin.js
+++ src/plugins/meetingminutes/plugin.js
@@ -5,9 +5,9 @@ export default async (app) => {
     await say(msg_events);
   }));
 }
-let msg_opener = await say(`Extra extra! Read all about it! Here are the most recent notes from the ` + date + ` ` + title + ` :rolled_up_newspaper:`);
-let msg_tldr = await say(`TLDR: ` + tldr);
-let msg_events = await say(`Events: ` + events);
+let msg_opener = `Extra extra! Read all about it! Here are the most recent notes from the ` + date + ` ` + title + ` :rolled_up_newspaper:`;
+let msg_tldr = `TLDR: ` + tldr;
+let msg_events = `Events: ` + events;
 // FIXME fetch parameters
 let title = await fetch(msg_title);
 let date = await fetch(msg_date);
