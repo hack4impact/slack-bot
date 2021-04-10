dad1fb2309201915f5efb47f098a67c341326e19
cmiller615 <carsenjakemiller@gmail.com>
2021-04-10 02:33:38 -0400

    meetingminutes notes for emily

--- src/plugins/meetingminutes/plugin.js
+++ src/plugins/meetingminutes/plugin.js
@@ -1,11 +1,14 @@
 export default async (app) => {
   app.message(((`Update me`), async ({message, say}) => {
-    await say(`Hello, <@${message.user}>! What a silly question. Try :bell_pepper: or :bacon: or heck, just :cheese:!!`)
+    await say(msg_opener);
+    await say(msg_tlder);
+    await say(msg_events);
   }));
 }
 let msg_opener = await say(`Extra extra! Read all about it! Here are the most recent notes from the ` + date + ` ` + title + ` :rolled_up_newspaper:`);
 let msg_tldr = await say(`TLDR: ` + tldr);
 let msg_events = await say(`Events: ` + events);
+// FIXME fetch parameters
 let title = await fetch(msg_title);
 let date = await fetch(msg_date);
 let tldr = await fetch(msg_tldr);
